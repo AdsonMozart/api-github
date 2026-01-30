431c87c50059556614506e5a53ae46b9ae063723
AdsonMozart <mozar7.dev@gmail.com>
2026-01-30 01:17:56 -0300

    feat: adding GitHub authentication token

--- src/main/java/br/com/mozar7/api_github/controller/GithubController.java
+++ src/main/java/br/com/mozar7/api_github/controller/GithubController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,14 @@ public class GithubController {
     private GithubClient githubClient;
 
     @GetMapping("repos")
-    public ResponseEntity<List<RepositoryResponse>> listRepos() {
-        return null;
+    public ResponseEntity<List<RepositoryResponse>> listRepos(@RequestHeader("token") String token) {
+
+        var repos = githubClient.listRepos(
+                "Bearer " + token,
+                null,
+                "public"
+        );
+
+        return ResponseEntity.ok(repos);
     }
 }
