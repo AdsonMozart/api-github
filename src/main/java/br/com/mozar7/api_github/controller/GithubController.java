840abb3fc6b16cee29403d61e2d65fa48b40ab1a
AdsonMozart <mozar7.dev@gmail.com>
2026-01-29 23:56:52 -0300

    feat: creating the interface httpClient to call the GitHub API

--- src/main/java/br/com/mozar7/api_github/controller/GithubController.java
+++ src/main/java/br/com/mozar7/api_github/controller/GithubController.java
@@ -1,5 +1,7 @@
 package br.com.mozar7.api_github.controller;
 
+import br.com.mozar7.api_github.client.GithubClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1")
 public class GithubController {
+
+    @Autowired
+    private GithubClient githubClient;
 
     @GetMapping("repos")
     public ResponseEntity<List<RepositoryResponse>> listRepos() {
