d6518f36ce18b7b7a02256137895487f9821de39
AdsonMozart <mozar7.dev@gmail.com>
2026-01-29 22:50:08 -0300

    chore: starting project

---
+++ src/test/java/br/com/mozar7/api_github/ApiGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mozar7.api_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
